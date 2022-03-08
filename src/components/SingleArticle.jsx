d1d4d74f5f39cc84f426c7c5001e2932579f134f
jjcrl <j412cff@icloud.com>
2022-03-08 16:09:35 +0000

    single article push for comments branch

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { fetchArticle } from "../utils/api";
 import { ArticleCard } from "./ArticleCard";
 import { Loader } from "./Loader";
+import { Comments } from "./Comments";
+import { ShowWrapper } from "./ShowWrapper";
 
 export const SingleArticle = () => {
   const [article, setArticle] = useState([]);
@@ -21,6 +23,10 @@ export const SingleArticle = () => {
   return (
     <div>
       <ArticleCard single={true} {...article} />
+      {/* <Voting id={id} votes={article.votes} /> */}
+      <ShowWrapper>
+        <Comments id={id} />
+      </ShowWrapper>
     </div>
   );
 };
